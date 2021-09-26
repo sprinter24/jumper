2d66e146ac33394418e7faf15e575af77344d1d2
sprinter24 <maks09pp@gmail.com>
2021-09-26 19:18:07 +0300

    -добавил липкую платформу

--- Assets/Scripts/StickySpot.cs
+++ Assets/Scripts/StickySpot.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class StickySpot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector2 normalGravity = new Vector2(0, -1);
+    private const float normalGravityScale = 20f;
+    private const float platformGravityScale = 50f;
     void Start()
     {
         
@@ -14,5 +16,37 @@ public class StickySpot : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        try
+        {
+            Rigidbody player = other.GetComponent<Rigidbody>();
+            player.angularVelocity = Vector3.zero;
+        }
+        catch
+        {
+            Debug.LogError("Somethinh wrong with sticky platform");
+        }
+        if (transform.rotation.z == 90)
+        {
+            Physics.gravity = new Vector3(-1, 0, 0) * normalGravityScale;
+        }
+        else if (transform.rotation.z == 270)
+        {
+            Physics.gravity = new Vector3(1, 0, 0) * normalGravityScale;
+        }
+        else
+        {
+            Physics.gravity = new Vector2(Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.Deg2Rad), -Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad)) * platformGravityScale;
+            Debug.Log(Physics.gravity);
+            Debug.Log(new Vector2(Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad), -Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.Deg2Rad)));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Physics.gravity = normalGravity * normalGravityScale;
     }
 }
