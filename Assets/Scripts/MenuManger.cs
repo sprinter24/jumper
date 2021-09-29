a6266d3d2bbb6dfe6d5828b1392e88a95d080469
sprinter24 <maks09pp@gmail.com>
2021-09-29 19:21:09 +0300

    добавил меню

---
+++ Assets/Scripts/MenuManger.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManger : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject menuForm;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        if (!gameIsPaused)
+        {
+            OpenMenu();
+        }
+        else
+        {
+            CloseMenu();
+        }
+    }
+
+    public void OpenMenu()
+    {
+        menuForm.SetActive(true);
+        PauseGame();
+    }
+
+
+    public void CloseMenu()
+    {
+        menuForm.SetActive(false);
+        ResumeGame();
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void Exit()
+    {
+        Debug.LogError("пидорас гнида ебаная всю игру сломал");
+    }
+}
---
+++ Assets/Scripts/PointsManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsManager : MonoBehaviour
+{
+    [SerializeField] private Text pointsBar;
+
+    [SerializeField] private Transform player;
+
+    [SerializeField] private float coordinatesForPoint = 1f;
+    private float lastPosition;
+
+    public int points = 0;
+    
+
+    void Start()
+    {
+        lastPosition = player.position.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(player.position.y >= lastPosition + coordinatesForPoint)
+        {
+            ++points;
+            lastPosition = player.position.y;
+            pointsBar.text = points.ToString();
+        }
+    }
+}
